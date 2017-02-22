4ef90487cb9b4a098c30285e37c4f2cfd7fb25bc
sween <ron.sweeney@gmail.com>
2017-02-22 12:44:15 -0500

    pages

---
+++ bleh.sh
@@ -0,0 +1,7 @@
+echo "# national_provider_directory" >> README.md
+git init
+mkdir docs
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sween/national_provider_directory.git
+git push -u origin master
